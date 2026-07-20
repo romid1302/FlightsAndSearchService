7750f8dcc90803ce11e7965f44e85171b73f6e50
romid1302 <cafe2romid@gmail.com>
2026-07-20 23:26:27 +0530

    Added City Repository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,54 @@
+const { City } = require('../models/index');
+
+class CityRepository {
+    async createCity({ name }){
+        try {
+            const city = await City.create({ name });
+            return city;
+
+        } catch (error) {
+            throw {error};
+
+        }
+    }
+
+    async deleteCity({ cityId}){
+        try{
+            await City.destroy({
+                where: {
+                    id: cityId
+                }
+            });
+        } catch(error){
+            throw {error};
+
+        }
+    }
+
+    async updateCity(cityId, data){
+         try{
+            const city = City.update(data,{
+                where : {
+                    id: cityId    
+                }
+            }
+            );
+            return city;
+
+        } catch(err){
+            throw {error};
+        }
+    }
+
+    async getCity({ cityId }){
+        try{
+            const city = City.findByPk(cityId);
+            return city;
+
+        } catch(err){
+            throw {error};
+        }
+    }
+}
+
+module.exports = CityRepository;
